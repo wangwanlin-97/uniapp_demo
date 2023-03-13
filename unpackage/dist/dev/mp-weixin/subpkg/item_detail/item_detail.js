935702bde42c9ab797fc2a74badb879ed6dbdf02
hatsune <1663037038@qq.com>
2023-03-13 18:30:51 +0800

    合并tabbar

---
+++ unpackage/dist/dev/mp-weixin/subpkg/item_detail/item_detail.js
@@ -0,0 +1,36 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["subpkg/item_detail/item_detail"],{
+
+/***/ 34:
+/*!**********************************************************************************************!*\
+  !*** F:/project/小程序/uni-demo/uni-demo/main.js?{"page":"subpkg%2Fitem_detail%2Fitem_detail"} ***!
+  \**********************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(wx, createPage) {
+
+var _interopRequireDefault = __webpack_require__(/*! @babel/runtime/helpers/interopRequireDefault */ 4);
+__webpack_require__(/*! uni-pages */ 26);
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 25));
+var _item_detail = _interopRequireDefault(__webpack_require__(/*! ./subpkg/item_detail/item_detail.vue */ 35));
+// @ts-ignore
+wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;
+createPage(_item_detail.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/wx.js */ 1)["default"], __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 2)["createPage"]))
+
+/***/ }),
+
+/***/ 35:
+/*!***************************************************************************!*\
+  !*** F:/project/小程序/uni-demo/uni-demo/subpkg/item_detail/item_detail.vue ***!
+  \***************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'F:\\project\\小程序\\uni-demo\\uni-demo\\subpkg\\item_detail\\item_detail.vue'");
+
+/***/ })
+
+},[[34,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/subpkg/item_detail/item_detail.js.map
